c38eb2cd345841db8a8348cda51dc84a75f58d0e
anushkahardkoretech-byte <anushka.hardkore.tech@gmail.com>
2026-03-20 12:01:46 +0530

    force redeploy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: true, // VERY IMPORTANT
-    allowedHosts: [
+    allowedHosts: [ 
       'unevasive-wryly-macie.ngrok-free.dev',
       '.ngrok-free.dev',
     ],
